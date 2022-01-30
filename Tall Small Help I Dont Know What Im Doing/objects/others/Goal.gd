a5789536aef84f38688577be03499848d3aef541
SnubbishShrub <93468999+SnubbishShrub@users.noreply.github.com>
2022-01-30 16:00:29 -0700

    Things

---
+++ Tall Small Help I Dont Know What Im Doing/objects/others/Goal.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+export (NodePath) var player
+export (NodePath) var player2
+export (String, FILE) var next_level
+
+func _process(delta):
+	if overlaps_body($player) or overlaps_body($player2):
+		get_tree().change_scene(next_level)
