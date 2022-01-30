8500cd8aad9862cc2d1fff2752fd1d1876f6ef42
SnubbishShrub <93468999+SnubbishShrub@users.noreply.github.com>
2022-01-29 18:44:11 -0700

    Camera Shake

--- Tall Small Help I Dont Know What Im Doing/ShakeCamera2D.gd
+++ Tall Small Help I Dont Know What Im Doing/ShakeCamera2D.gd
@@ -11,15 +11,27 @@ var trauma_power = 2  # Trauma exponent. Use [2, 3].
 var noise_y = 0
 onready var noise = OpenSimplexNoise.new()
 
+func _ready():
+	randomize()
+	noise.seed = randi()
+	noise.period = 4
+	noise.octaves = 2
+	BigJump.connect("bigJump", self, "add_trauma(0.2)")
+	
+func add_trauma(num):
+	trauma = min(trauma + num, 1.0)
+	
 func _process(delta):
+	if Input.is_action_just_pressed("ui_down"):
+		add_trauma(0.2)
 	if target:
 		global_position = get_node(target).global_position
 	if trauma:
 		trauma = max(trauma - decay * delta, 0)
 		shake()
 
-
 func shake():
+	var amount = pow(trauma, trauma_power)
 	noise_y += 1
 	rotation = max_roll * amount * noise.get_noise_2d(noise.seed, noise_y)
 	offset.x = max_offset.x * amount * noise.get_noise_2d(noise.seed*2, noise_y)
