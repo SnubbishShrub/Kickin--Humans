a0b3923e3247a9f2fcb0238cfd2232f224afd377
SnubbishShrub <93468999+SnubbishShrub@users.noreply.github.com>
2022-01-30 11:18:28 -0700

    Added Animation To Buttons And Levers

--- Tall Small Help I Dont Know What Im Doing/objects/others/Lever.gd
+++ Tall Small Help I Dont Know What Im Doing/objects/others/Lever.gd
@@ -2,24 +2,35 @@ extends Node2D
 
 
 export var state = "off"
+export var red = true
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if state == "off":
-		$"Lever Sprite".flip_h = false
+		if red == true:
+			$AnimationPlayer.play("Red Off")
+		else:
+			$AnimationPlayer.play("Blue Off")
 	else:
-		$"Lever Sprite".flip_h = true
+		if red == true:
+			$AnimationPlayer.play("Red On")
+		else:
+			$AnimationPlayer.play("Blue On")
 		
 func _process(_delta):
 	if ($Area2D.overlaps_body($"../../../Tall Guy") and Input.is_action_just_pressed("ui_down")) or \
 	($Area2D.overlaps_body($"../../../Small Guy") and Input.is_action_just_pressed("2_topple")):
 		if state == "off":
 			state = "on"
+			if red == true:
+				$AnimationPlayer.play("Red On")
+			else:
+				$AnimationPlayer.play("Blue On")
 		else:
 			state = "off"
+			if red == true:
+				$AnimationPlayer.play("Red Off")
+			else:
+				$AnimationPlayer.play("Blue Off")
 		GlobalLever.LeverChanged()
-	if state == "off":
-		$"Lever Sprite".flip_h = false
-	else:
-		$"Lever Sprite".flip_h = true
