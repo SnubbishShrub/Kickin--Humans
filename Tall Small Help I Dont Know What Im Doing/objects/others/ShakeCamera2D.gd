f3276eac13f0788455e992c37468972d5981ae6f
SnubbishShrub <93468999+SnubbishShrub@users.noreply.github.com>
2022-03-02 14:36:30 -0700

    Camera Zoom

--- Tall Small Help I Dont Know What Im Doing/objects/others/ShakeCamera2D.gd
+++ Tall Small Help I Dont Know What Im Doing/objects/others/ShakeCamera2D.gd
@@ -4,11 +4,16 @@ extends Camera2D
 export var decay = 0.8 
 export var max_offset = Vector2(100, 75) 
 export var max_roll = 0.1 
-export (NodePath) var target  
+export (NodePath) var small_guy
+export (NodePath) var tall_guy    
 
 var trauma = 0.0 
 var trauma_power = 2 
 var noise_y = 0
+var distance_y
+var distance_x
+var zoom_factor
+
 onready var noise = OpenSimplexNoise.new()
 
 func _ready():
@@ -20,12 +25,27 @@ func _ready():
 	KickAndBreak.connect("Kicked", self, "add_trauma")
 	KickAndBreak.connect("Break", self, "add_trauma")
 	
+	small_guy = get_node(small_guy)
+	tall_guy = get_node(tall_guy)
+	
 func add_trauma(num):
 	trauma = min(trauma + num, 1.0)
 	
 func _process(delta):
-	if target:
-		global_position = get_node(target).global_position
+	global_position = small_guy.global_position
+	global_position.y = global_position.y - 50
+	distance_y = abs(tall_guy.position.y - small_guy.position.y)
+	distance_x = abs(tall_guy.position.x - small_guy.position.x)
+	
+	if distance_x >= 200:
+		zoom_factor = 1 + (0.004 * (distance_x - 199))
+	elif distance_y >= 100:
+		zoom_factor = 1 + (0.004 * (distance_y - 99))
+	else:
+		zoom_factor = 1
+		
+	zoom = Vector2(zoom_factor, zoom_factor)
+	
 	if trauma:
 		trauma = max(trauma - decay * delta, 0)
 		shake()
